2d22030196475810172ca7d1d86fd3de79556cd7
ppscon <ppearson@me.com>
2025-04-22 18:24:58 +0100

    chore: Remove non-essential files and directories

--- Data/qvs_sample_files/md5_example.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-function generateMD5Hash(data) {
-    return crypto.createHash('md5').update(data).digest('hex');
-}
-
-console.log(generateMD5Hash('Test data'));
--- Data/qvs_sample_files/md5_example.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def generate_md5_hash(data):
-    md5_hash = hashlib.md5()
-    md5_hash.update(data.encode('utf-8'))
-    return md5_hash.hexdigest()
-
-print(generate_md5_hash("Test data"))
