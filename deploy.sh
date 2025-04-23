f773cb82bc83f22dfda114815804b06dccd7c1de
ppscon <ppearson@me.com>
2025-04-23 17:32:23 +0100

    Revert recent deployment changes to restore working configuration

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copy the Linux version of the scanner to the scanner directory
-echo "Setting up scanner..."
-mkdir -p scanner
-cp scanner/qvs-pro-linux scanner/qvs-pro
-chmod +x scanner/qvs-pro
-
-echo "Scanner setup complete!" 
